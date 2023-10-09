887b73876944c461cc82876d957d92f61730f8d9
yoyos <yoyosharp89@gmail.com>
2023-10-09 10:48:55 +0700

    login page

--- src/main/java/com/fx23121/DonationPlatform/Controller/HomeController.java
+++ src/main/java/com/fx23121/DonationPlatform/Controller/HomeController.java
@@ -1,10 +1,12 @@
 package com.fx23121.DonationPlatform.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/")
+@Controller
 public class HomeController {
 
+    @RequestMapping("/")
     public String showHome() {
         return "admin/home";
     }
